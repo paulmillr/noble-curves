1d5286ffa718e8bccb0e0564ec8c1aa70bb6ff27
Paul Miller <paul@paulmillr.com>
2023-08-27 16:49:42 +0000

    single-file build: expose more methods

--- build/input.js
+++ build/input.js
@@ -1,8 +1,17 @@
 import { bytesToHex, concatBytes, hexToBytes } from '@noble/curves/abstract/utils';
 
-export { secp256k1 } from '@noble/curves/secp256k1';
-export { ed25519, x25519 } from '@noble/curves/ed25519';
-export { ed448, x448 } from '@noble/curves/ed448';
+export { secp256k1, schnorr as secp256k1_schnorr } from '@noble/curves/secp256k1';
+export {
+  ed25519,
+  x25519,
+  edwardsToMontgomeryPub as ed25519_edwardsToMontgomeryPub,
+  edwardsToMontgomeryPriv as ed25519_edwardsToMontgomeryPriv,
+} from '@noble/curves/ed25519';
+export {
+  ed448,
+  x448,
+  edwardsToMontgomeryPub as ed448_edwardsToMontgomeryPub,
+} from '@noble/curves/ed448';
 export { p256 } from '@noble/curves/p256';
 export { p384 } from '@noble/curves/p384';
 export { p521 } from '@noble/curves/p521';
