91740efad68fe8a318b41ea46312c75a8ca9eafd
Paul Miller <paul@paulmillr.com>
2024-11-21 10:26:44 +0000

    utils.bytesToHex: small change

--- src/abstract/utils.ts
+++ src/abstract/utils.ts
@@ -58,11 +58,11 @@ export function hexToNumber(hex: string): bigint {
 }
 
 // We use optimized technique to convert hex string to byte array
-const asciis = { _0: 48, _9: 57, _A: 65, _F: 70, _a: 97, _f: 102 } as const;
+const asciis = { _0: 48, _9: 57, A: 65, F: 70, a: 97, f: 102 } as const;
 function asciiToBase16(ch: number): number | undefined {
   if (ch >= asciis._0 && ch <= asciis._9) return ch - asciis._0; // '2' => 50-48
-  if (ch >= asciis._A && ch <= asciis._F) return ch - (asciis._A - 10); // 'B' => 66-(65-10)
-  if (ch >= asciis._a && ch <= asciis._f) return ch - (asciis._a - 10); // 'b' => 98-(97-10)
+  if (ch >= asciis.A && ch <= asciis.F) return ch - (asciis.A - 10); // 'B' => 66-(65-10)
+  if (ch >= asciis.a && ch <= asciis.f) return ch - (asciis.a - 10); // 'b' => 98-(97-10)
   return;
 }
 
