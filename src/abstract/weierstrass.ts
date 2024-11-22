867b21ed9210ee022bd974e92f9a0214a1722d4d
Paul Miller <paul@paulmillr.com>
2024-11-22 12:21:33 +0000

    weierstrass: prohibit messages longer than 8KB

--- src/abstract/weierstrass.ts
+++ src/abstract/weierstrass.ts
@@ -1010,6 +1010,8 @@ export function weierstrass(curveDef: CurveType): CurveFn {
   const bits2int =
     CURVE.bits2int ||
     function (bytes: Uint8Array): bigint {
+      // Our custom check "just in case"
+      if (bytes.length > 8192) throw new Error('input is too large');
       // For curves with nBitLength % 8 !== 0: bits2octets(bits2octets(m)) !== bits2octets(m)
       // for some cases, since bytes.length * 8 is not actual bitLength.
       const num = ut.bytesToNumberBE(bytes); // check for == u8 done here
