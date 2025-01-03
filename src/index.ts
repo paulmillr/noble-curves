7a808236d36b53e69fe4ddc054bfd54799a7acf8
Paul Miller <paul@paulmillr.com>
2025-01-03 00:44:57 +0000

    Add docs for root module.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
+/**
+ * Audited & minimal JS implementation of elliptic curve cryptography. Check out individual modules.
+ * @module
+ */
 throw new Error('root module cannot be imported: import submodules instead. Check out README');
