fbe7a058720fa9e8b9777eb4048e3e7c04be66ec
Paul Miller <paul@paulmillr.com>
2025-01-18 01:37:22 +0000

    Bump jsbt helpers 0.3.0. Add build:release script.

--- build/README.md
+++
@@ -1,7 +0,0 @@
-# build
-
-The directory is used to build a single file which contains everything.
-
-The single file uses iife wrapper and can be used in browsers as-is.
-
-Don't use it unless you can't use NPM/ESM, which support tree shaking.
--- build/input-single.js
+++
@@ -1,3 +0,0 @@
-import { bytesToHex, hexToBytes, concatBytes, utf8ToBytes } from '@noble/curves/abstract/utils';
-export { ed25519, x25519 } from '@noble/curves/ed25519';
-export const utils = { bytesToHex, hexToBytes, concatBytes, utf8ToBytes, randomBytes };
