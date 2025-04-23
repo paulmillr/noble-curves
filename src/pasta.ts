b7e379151b04e99fb1177c09826c87f117cd11c1
Paul Miller <paul@paulmillr.com>
2025-04-23 00:29:37 +0000

    Deprecate pasta

--- src/pasta.ts
+++ src/pasta.ts
@@ -1,1 +1,5 @@
-export { pallas, vesta } from './misc.ts';
+import { pallas as pn, vesta as vn } from './misc.ts';
+/** @deprecated */
+export const pallas: typeof pn = pn;
+/** @deprecated */
+export const vesta: typeof vn = vn;
