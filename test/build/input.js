9c89dc43143007fe91ee0529a9e4fa0365547a53
Paul Miller <paul@paulmillr.com>
2025-08-09 08:14:31 +0000

    Export more in single-file build

--- test/build/input.js
+++ test/build/input.js
@@ -1,14 +1,25 @@
-import { bytesToHex, concatBytes, hexToBytes, utf8ToBytes } from '@noble/curves/utils.js';
 export { bls12_381 } from '@noble/curves/bls12-381.js';
 export { bn254 } from '@noble/curves/bn254.js';
-export {
-  ed25519, x25519
-} from '@noble/curves/ed25519.js';
-export {
-  ed448, x448
-} from '@noble/curves/ed448.js';
+export { ed25519, ed25519ctx, ed25519ph, ristretto255, x25519 } from '@noble/curves/ed25519.js';
+export { decaf448, ed448, ed448ph, x448 } from '@noble/curves/ed448.js';
+export { babyjubjub, brainpoolP256r1, brainpoolP384r1, brainpoolP512r1, jubjub } from '@noble/curves/misc.js';
 export { p256, p384, p521 } from '@noble/curves/nist.js';
-export { secp256k1, schnorr as secp256k1_schnorr } from '@noble/curves/secp256k1.js';
+export { schnorr, secp256k1 } from '@noble/curves/secp256k1.js';
 
-export * as mod from '@noble/curves/abstract/modular.js';
-export const utils = { bytesToHex, concatBytes, hexToBytes, utf8ToBytes };
+// hash-to-curve
+export { ristretto255_hasher } from '@noble/curves/ed25519.js';
+export { decaf448_hasher } from '@noble/curves/ed448.js';
+export { p256_hasher, p384_hasher, p521_hasher } from '@noble/curves/nist.js';
+export { secp256k1_hasher } from '@noble/curves/secp256k1.js';
+
+// OPRFs
+export { ristretto255_oprf } from '@noble/curves/ed25519.js';
+export { decaf448_oprf } from '@noble/curves/ed448.js';
+export { p256_oprf, p384_oprf, p521_oprf } from '@noble/curves/nist.js';
+
+import { bytesToHex, concatBytes, hexToBytes } from '@noble/curves/utils.js';
+export const utils = { bytesToHex, concatBytes, hexToBytes };
+
+export { Field } from '@noble/curves/abstract/modular.js';
+export * as webcrypto from '@noble/curves/webcrypto.js';
+
